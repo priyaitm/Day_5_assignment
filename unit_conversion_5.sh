019997ef12ea69a7292ff2aab868c249a5847db7
priya <priyachauhan47365@gmail.com>
2022-12-06 01:20:19 -0600

    Added: Unit conversion

---
+++ unit_conversion_5.sh
@@ -0,0 +1,10 @@
+echo " 1in = 1/12 ft"
+echo " 42 in =42*1/12 ft"
+echo $((42*1/12))" ft"
+echo " 1feet=0.305meter"
+echo $((2400*93025/1000000))"  square meters"
+echo " 1 arce=4040.86 square meter "
+Area=$((  25*223))
+echo $Area "square meter"
+
+echo " 1 arce =" $(( 5575*404086/100)) "square meter"
