b7b27d7b33fd1db2eae9012635a21a6b683439a5
priya <priyachauhan47365@gmail.com>
2022-12-06 08:47:05 -0600

    Added: day5 prob2 case problem

---
+++ single_digit_num_word_case_1.sh
@@ -0,0 +1,20 @@
+read -p " Enter Any Single Digit Number To Get It In Words: " num
+case $num in
+1) echo "One"
+;;
+2) echo "Two"
+;;
+3) echo  "Three"
+;;
+4) echo "Four"
+;;
+5) echo  "Five"
+;;
+6) echo  "Six"
+;;
+7) echo "Seven"
+;;
+8) echo  "Eight"
+;;
+*) echo "Nine"
+esac
