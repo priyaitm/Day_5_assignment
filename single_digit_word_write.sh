986c98691079f93673515f8494ca3f116a26c12c
priya <priyachauhan47365@gmail.com>
2022-12-06 07:36:15 -0600

    Added:Day5 prob 2 use of if and elif

---
+++ single_digit_word_write.sh
@@ -0,0 +1,34 @@
+read -p "Enter single digit:" num
+if((num==0))
+      then
+           echo "ZERO"
+elif((num==1))
+       then
+            echo "ONE"
+elif((num==2))
+       then
+            echo "TWO"
+elif((num==3))
+       then
+            echo "THREE"
+elif((num==4))
+       then
+             echo "FOUR"
+elif((num==5))
+       then
+            echo "FIVE"
+elif((num==6))
+      then
+             echo "SIX"
+elif((num==7))
+    then
+           echo "SEVEN"
+elif((num==8))
+    then
+          echo  " EIGHT"
+   else
+         echo "NINE"
+            
+
+fi
+
